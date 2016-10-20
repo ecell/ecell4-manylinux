814a6f9332dcc9a9481d07c178bd5cdfda57c038
Kozo Nishida <kozo.nishida@gmail.com>
2016-10-20 17:08:09 +0900

    replace wget to curl (for downloading hdf5)

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,8 @@ yum update -y
 yum install cmake gsl-devel -y
 
 wget http://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_1_59_0.tar.gz
-wget http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.14/bin/linux-centos5-x86_64-gcc482/hdf5-1.8.14-linux-centos5-x86_64-gcc482-shared.tar.gz
+#wget http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.14/bin/linux-centos5-x86_64-gcc482/hdf5-1.8.14-linux-centos5-x86_64-gcc482-shared.tar.gz
+curl -L http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.14/bin/linux-centos5-x86_64-gcc482/hdf5-1.8.14-linux-centos5-x86_64-gcc482-shared.tar.gz -o hoge.tar.gz
 
-tar xf hdf5-1.8.14-linux-centos5-x86_64-gcc482-shared.tar.gz
+tar xf hoge.tar.gz
 tar xf boost_1_59_0.tar.gz
